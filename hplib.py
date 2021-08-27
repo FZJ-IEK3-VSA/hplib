7f8bcabeae52281448a06b87610d36f9670aa762
xihaui <75681123+xihaui@users.noreply.github.com>
2021-08-27 11:47:05 +0200

    added Lineplots for validation

--- hplib.py
+++ hplib.py
@@ -88,3 +88,52 @@ def simulate(T_in,T_out,parameters):
             Pth=Pth_ref
     
     return Pth,Pel,COP
+
+def simulate_normalized(T_in,T_out,parameters):
+    #input  x -> T_in [°C]
+    #       y -> T_out [°C]
+    #       parameters -> list from function getParameters()
+    
+    x=T_in
+    y=T_out
+    Group=parameters['Group'].array[0]
+    k4=parameters['p1_P_el [1/°C]'].array[0]
+    k5=parameters['p2_P_el [1/°C]'].array[0]
+    k6=parameters['p3_P_el [-]'].array[0]
+    k7=parameters['p1_COP [-]'].array[0]
+    k8=parameters['p2_COP [-]'].array[0]
+    k9=parameters['p3_COP [-]'].array[0]
+    Pel_ref=parameters['P_el_ref [W]'].array[0]
+    Pth_ref=parameters['P_th_ref [W]'].array[0]
+    # for subtype = Inverter
+    if Group==1 or Group==2 or Group==3:
+        COP=k7*x+k8*y+k9
+        if x>=5: #minimum electrical Power at 5°C
+            x=5
+        Pel=(k4*x+k5*y+k6)#*Pel_ref
+        Pth=Pel*COP
+        if COP<=1:
+            COP=1
+            Pel=1#Pth_ref
+            Pth=1#Pth_ref
+        
+    # for subtype = On-Off
+    elif Group==4 or Group==5 or Group==6:
+        Pel=(k4*x+k5*y+k6)#*Pel_ref
+        COP=k7*x+k8*y+k9
+        Pth=Pel*COP
+        if COP<=1:
+            COP=1
+            Pel=1#Pth_ref
+            Pth=1#Pth_ref
+    # for subtype = Two-stages
+    else:
+        Pel=0
+        COP=0
+        Pth=0
+        if COP<=1:
+            COP=1
+            Pel=1#Pth_ref
+            Pth=1#Pth_ref
+    
+    return Pth,Pel,COP
