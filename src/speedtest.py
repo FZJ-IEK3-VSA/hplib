a3f4aa79fbeb7a48b5fd70a825dd4bdb161729a8
Kai Rösken <kroesken71@gmail.com>
2021-10-29 14:35:58 +0200

    Update speedtest.py

--- src/speedtest.py
+++ src/speedtest.py
@@ -4,11 +4,11 @@ import numpy as np
 import time
 
 # load parameters
-parameters = hpl.get_parameters(model='Generic', group_id=1, t_in=-7, t_out=52, p_th=10000)
+parameters = hpl.get_parameters(model='i-SHWAK V4 06', group_id=1, t_in=-7, t_out=52, p_th=10000)
 
 # load one year input data, 1min time resolution
-df = pd.read_csv('../input/TestYear.csv')
-df['T_amb'] = df['T_in_primary'] # air/water heat pump -> T_amb = T_in_primary
+temperature = pd.read_csv('../input/TestYear.csv')
+temperature['T_amb'] = temperature['T_in_primary'] # air/water heat pump -> T_amb = T_in_primary
 
 # Old way
 """
@@ -50,9 +50,18 @@ results = heat_pump.simulate(t_in_primary=5, t_in_secondary=30, t_amb=5)
 end = time.time()
 print('Dauer Einzelaufruf mit neuem Konzept = '+str(end-start)+' Sekunden')
 
+temp = temperature['T_in_primary'].to_numpy()
+start = time.time()
+results = heat_pump.simulate(temperature['T_in_primary'].to_numpy(),
+                             temperature['T_in_secondary'].to_numpy(),
+                             temperature['T_amb'].to_numpy())
+end = time.time()
+print('Dauer Array mit neuem Konzept = '+str(end-start)+' Sekunden')
+
+
 i = 0
 start = time.time()
-for a, b, c in zip(df['T_in_primary'].values, df['T_in_secondary'].values, df['T_amb'].values):
+for a, b, c in zip(temperature['T_in_primary'].values, temperature['T_in_secondary'].values, temperature['T_amb'].values):
     i = i + 1
     results = heat_pump.simulate(t_in_primary=a, t_in_secondary=b, t_amb=c)
 end = time.time()
